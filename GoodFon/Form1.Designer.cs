a0a4123b2e42f44257821cacfaca90adc953797a
slfl <slfl@mail.ru>
2014-02-10 12:18:24 +0300

    Parsing
    Скрипты на парсинг

--- GoodFon/Form1.Designer.cs
+++ GoodFon/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.Button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -42,6 +43,8 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(410, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "http://www.goodfon.ru/catalog/girls/";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // progressBar1
             // 
@@ -80,6 +83,15 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "1";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // GoodFon
             // 
             this.AllowDrop = true;
@@ -87,6 +99,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(434, 162);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.Button1);
@@ -113,6 +126,7 @@
         private System.Windows.Forms.Button Button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- GoodFon/Form1.cs
+++ GoodFon/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.IO;
+using System.Net;
+
+
+
 
 namespace WindowsFormsApplication1
 {
@@ -18,6 +24,16 @@ namespace WindowsFormsApplication1
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //Ссылка на страницу
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) //Кол-во страниц
         {
 
         }
@@ -35,7 +51,99 @@ namespace WindowsFormsApplication1
 
         private void button3_Click(object sender, EventArgs e) //Кнопка старта
         {
-            MessageBox.Show("Ну что, программа начала работать!");
+            List<string> images = ParseLinks(textBox1.Text, Convert.ToInt32(textBox1.Text));
+            foreach (string a in images)
+            {
+                //MessageBox.Show("Ну что, программа начала работать!");
+                DownloadImage(GetImage(a));
+            }
+        }
+
+        // А эта функция принимает в качестве аргументов ссылку на категорию и количество страниц для парсинга
+        public List<string> ParseLinks(string category, int pages)
+        {
+            List<string> links = new List<string>();
+
+            for (int i = 1; i <= pages; ++i)
+            {
+                HtmlAgilityPack.HtmlDocument HD = new HtmlAgilityPack.HtmlDocument();
+                var web = new HtmlWeb
+                {
+                    AutoDetectEncoding = false,
+                    OverrideEncoding = Encoding.UTF8,
+                };
+                HD = web.Load(String.Format("{0}/index-{1}.html", category, i));
+
+                HtmlNodeCollection images = HD.DocumentNode.SelectNodes("//div[@class='tabl_td']");
+                if (images != null)
+                {
+                    foreach (HtmlNode node in images)
+                    {
+                        HtmlNodeCollection link_tmp = node.SelectNodes("//a[@itemprop='url']");
+
+                        foreach (HtmlNode node_tmp in link_tmp)
+                        {
+                            links.Add(node_tmp.Attributes["href"].Value.ToString());
+                        }
+                    }
+                }
+            }
+
+            return links;
+        }
+
+        public string GetImage(string url)
+        {
+            HtmlAgilityPack.HtmlDocument HD = new HtmlAgilityPack.HtmlDocument();
+            var web = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8,
+            };
+            HD = web.Load(url);
+
+            string imgurl = "";
+            HtmlNodeCollection images = HD.DocumentNode.SelectNodes("//a[@target='_blank']");
+            if (images != null)
+            {
+                foreach (HtmlNode node in images)
+                {
+                    if (node.Attributes["href"].Value.ToString().Contains("download.php"))
+                    {
+                        imgurl = node.Attributes["href"].Value.ToString();
+                        break;
+                    }
+                }
+            }
+
+            HtmlAgilityPack.HtmlDocument HD2 = new HtmlAgilityPack.HtmlDocument();
+            var web1 = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8,
+            };
+            HD2 = web1.Load(String.Format("http://www.goodfon.ru/{0}", imgurl));
+
+            HtmlNodeCollection image_final = HD2.DocumentNode.SelectNodes("//img[@border='1']");
+            if (image_final != null)
+            {
+                foreach (HtmlNode node in image_final)
+                {
+                    return node.Attributes["src"].Value.ToString();
+                }
+            }
+
+            return "UNDERFINED";
+        }
+
+        public void DownloadImage(string img)
+        {
+            DirectoryInfo info = new DirectoryInfo("/");
+            long uniqueKey = info.LastWriteTime.Ticks + 1L;
+            string filename = Guid.NewGuid() + ".jpg";
+
+            WebClient webclient = new WebClient();
+            webclient.DownloadFile(img, filename);
         }
     }
 }
